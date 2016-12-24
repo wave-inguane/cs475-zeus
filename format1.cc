3f56fb1017ada387ccd47badb780bcb2c2b4c558
Wave <wavejaz1@hotmail.com>
2016-12-24 01:03:39 -0500

    format output

---
+++ format1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char** argv){
+		long pop1 = 2425785, pop2=47, pop3 =9761;
+
+	cout << setw(8) << "LOCATION" << setw(12)
+		<< "POPULATION" << 
+    setw(8) << pop1 << endl;
+		return 0;
+}
+
